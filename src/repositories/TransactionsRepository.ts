b97ee7e797b51c9c8179c7d8e3d16b6ea518fe07
lopessoftec <lopessoftec@gmail.com>
2020-08-30 17:25:45 -0300

    finishing challenge fundamentals nodejs

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (total, transaction) =>
+        total + (transaction.type === 'income' ? transaction.value : 0),
+      0,
+    );
+    const outcome = this.transactions.reduce(
+      (total, transaction) =>
+        total + (transaction.type === 'outcome' ? transaction.value : 0),
+      0,
+    );
+
+    const total = income - outcome;
+
+    return {
+      total,
+      income,
+      outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
